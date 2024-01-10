27382dd6aa6738e60c3618443d25c49456e9c8f6
Vivek Sahu <viveksahu23@gmail.com>
2024-01-10 19:46:46 +0530

    sidebar and feed page

---
+++ src/components/Feed.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+
+import { Stack, Typography, Box } from "@mui/material";
+import { SideBar, Videos } from "./";
+import { fetchAPI } from "../utils.js/fetchAPI";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchAPI(`search?part=snippet&q=${selectedCategory}`);
+      setVideos(data);
+    };
+    fetchData();
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 Vivek Sahu
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: "white",
+          }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
---
+++ src/utils.js/fetchAPI.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: "50",
+    regionCode: "IN",
+  },
+  headers: {
+    "X-RapidAPI-Key": "883bf88f3emshe3d9242fe0223b8p1e3ef1jsn483e522bf7d1",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchAPI = async (url) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data.items;
+  } catch (error) {
+    console.error(error);
+  }
+};
