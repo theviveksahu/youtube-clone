959180e8830b7256cabda1f8830551d580e84c9f
Vivek Sahu <viveksahu23@gmail.com>
2024-01-13 21:39:33 +0530

    video player and related videos

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -21,7 +21,7 @@ const VideoCard = ({
   return (
     <Card
       sx={{
-        width: { md: "320px", xs: "100%" },
+        width: { xs: "100%", sm: "358px", md: "320px" },
         boxShadow: "none",
         borderRadius: 0,
       }}
@@ -31,7 +31,7 @@ const VideoCard = ({
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
           sx={{
-            width: "358px",
+            width: { xs: "100%", sm: "358px", md: "320px" },
             height: "180px",
           }}
         />
